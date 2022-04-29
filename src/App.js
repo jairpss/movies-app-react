d4d2421e7d517bf72ff749635b89f8e1b790cc3d
Jair Partida <jairpartida1@gmail.com>
2022-04-28 22:13:53 -0600

    connecting the API to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react'
 
-function App() {
+//API key  343d649a
+const API_URL = 'https://www.omdbapi.com?apikey=343d649a'
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>App</h1>
   );
 }
 
